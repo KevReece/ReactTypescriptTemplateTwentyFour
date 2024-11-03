d10be59dcbac8c8505d3cf5603415e50e4e2f199
Kev Reece <kevinjamesreece@gmail.com>
2024-11-03 13:30:49 +0100

    feat: Boilerplate

---
+++ upvote/src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <h1>Upvote</h1>
+      <div className="card">
+        <p>
+          Upvote item
+        </p>
+      </div>
+    </>
+  )
+}
+
+export default App
